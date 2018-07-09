0cb84bc49b5af22363259c64c97271611f2b71fc
Alex Zdatny <alexzdatny@gmail.com>
2018-07-09 00:10:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,17 @@ class ApplicationController < Sinatra::Base
 
   get '/'
     erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
 end
