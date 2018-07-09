a3b431bbc7b0aebb4d6c7f803c3b053183673562
Alex Zdatny <alexzdatny@gmail.com>
2018-07-09 00:58:00 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     end
 
     get '/login' do
-      if !logged_in?
+      if logged_in?
         erb :'users/login'
       else
         redirect to '/tweets'
